95546d80f25443dc1392c5cef01513bc0dc6aa74
Tom Storm <tstorm@invoca.com>
2018-03-16 22:12:50 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed = "Mutt")
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+
+
+
+
+
